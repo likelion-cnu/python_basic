e59e0f5f240dc299301753e0f9c41e78ce4e1275
jin-07 <102872473+jin-07@users.noreply.github.com>
2022-05-03 04:34:21 +0900

    Update 2.py

--- 2.py
+++ 2.py
@@ -2,13 +2,16 @@ order={"Num": "101", "Buger":"치즈버거","Beverage":"콜라"}
 
 for key, value in order.items():
     print(key, value, sep=' : ')
+print()
 
 order["Burger"]="더블치즈버거"
 order["Extra"]="치킨너겟"
 print(order)
+print()
 
 order.clear()
 print(order)
+print()
 
 order = [{"Num":"102", "Burger":"불고기버거세트", "Beverage" : "바닐라쉐이크"},
          {"Num":"103", "Burger":"치킨버거", "Beverage":"딸기칠러", "Extra":"치즈스틱"}]
