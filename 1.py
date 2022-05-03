02255f7cf4ab58a5c2f7780a4d9b3b5c49d1b47c
rktdnjs <rktdnjs@naver.com>
2022-05-03 12:20:47 +0900

    신효원의 파이썬 기초 과제

--- 1.py
+++ 1.py
@@ -3,23 +3,22 @@ import random
 
 booklist = [] #빈 리스트 생성
 
-def enrollment_book():
-    
+def enrollment_book(): #책 등록 함수
     enrollment_name = input("책 이름은? ")
     enrollment_author = input("책 저자는? ")
     enrollment_publisher = input("책 출판사는? ")
     enrollment_day = input("책 입고일은? 8숫자로 입력해주세요. ex:20220425 >> ")
     booklist.append({"제목":enrollment_name, "저자":enrollment_author,
-    "출판사":enrollment_publisher, "입고일":enrollment_day})
+    "출판사":enrollment_publisher, "입고일":enrollment_day}) #딕셔너리로 생성하여 추가
     print("책 등록이 완료되었습니다. 메뉴로 돌아갑니다.")
 
-def delete_book(): #삭제처리를 무수히 많이 고민해보았으나...전부다 실패 ㅠㅠ 그외에는 구현완료
-    for i in booklist:
+def delete_book(): #책 삭제 함수
+    for i in booklist: #삭제하기 전에 모든 책의 목록을 보여줌
         print(i["제목"])
     select_book = input("삭제할 책 이름을 입력해주세요. ")
-    for i in booklist:
-        if select_book == i["제목"]:
-            print(booklist) #책 데이터제거 완료후 체크
+    for i in range(len(booklist)): #booklist의 길이만큼 리스트에 접근
+        if booklist[i]["제목"] == select_book: #해당하는 제목과 삭제하려는 책이 같은경우
+            del booklist[i] #해당하는 순서의 데이터를 삭제함
     print("책 삭제가 완료되었습니다. 메뉴로 돌아갑니다.")
 
 while True:
@@ -30,10 +29,10 @@ while True:
     print("5. 종료")
     menu_select = input("메뉴 선택: ")
 
-    if menu_select == "1": #입력값(menu_select가 1일 때)
+    if menu_select == "1": #책 등록 항목
         enrollment_book()
 
-    if menu_select == "2": #책 삭제 항목(미완성)
+    if menu_select == "2": #책 삭제 항목
         delete_book()
 
     if menu_select == "3": #책 목록 출력
